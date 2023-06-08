dd68e24cea512f52c8fe696a6667196b4ae6602f
JonaCardozoo <futjona456@gmail.com>
2023-06-08 09:43:38 -0300

    segundo arreglo

--- actividad-parcial/actividad-parcial/Program.cs
+++ actividad-parcial/actividad-parcial/Program.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace actividad_parcial {
     internal class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args) 
+        {
+
+            promedio p = new promedio();
+
+            double num = 0;
+            int op=0;
+            while(op != -1) 
+                
+            {
+                
+                Console.WriteLine("\t1- Agregar numero");
+                Console.WriteLine("\t2- Calcular Promedio");
+                Console.WriteLine("\t3- Salir");
+                op = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();    
+                
+                switch(op) {
+
+                case 1:
+                    
+                    
+
+                    while(num != -1) 
+                        
+                    {
+                        
+                        Console.WriteLine("Ingrese los numeros (corte con -1)");
+                        num = Convert.ToDouble(Console.ReadLine());
+                        p.IngresarNum(num);
+                        p.promedioo(num);
+                    }
+
+                    
+                    break;
+
+                case 2:
+
+                    
+
+                    p.CalcularPromedio(num);
+                    Console.WriteLine("El promedio es de: {0:f2}", p.CalcularPromedio(num));
+
+                    break;
+                
+                
+                }
+                
+                
+                
+
+
+
+
+            }
         }
     }
 }
---
+++ actividad-parcial/actividad-parcial/promedio.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_parcial {
+    internal class promedio {
+
+
+        public double num;
+        public double contador;
+        public double acum;
+
+        
+
+        public void IngresarNum(double num) {
+
+            this.num = num;
+            
+        }
+
+
+        
+        public double CalcularPromedio(double num) 
+        {
+
+            
+            return acum / contador;
+
+
+        }
+
+
+        public void promedioo(double num) {
+
+
+            if(num > 0) {
+
+                contador++;
+                acum += num;
+
+            }
+
+        }
+
+    }
+}
