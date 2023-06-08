3c72390881f5ad2cd831010a537a9b70faac799a
jonacardozoo <futjona456@gmail.com>
2023-06-08 08:43:30 -0300

    iniciando proyecto

---
+++ actividad-parcial/actividad-parcial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_parcial {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
